1ebe476ff99f24360ec295dbfd44f7194cff86da
Harry McCleave <harry@mccleave.ca>
2021-10-27 12:45:23 -0700

    Construct Unittests refactor (consolidate XSmall, XLarge, XAsymmetric -> X)

--- src/construct_unittests.cpp
+++ src/construct_unittests.cpp
@@ -19,54 +19,57 @@ TEST_F(DataTypeOperationTrackingSBOVector, MustDefaultConstructWithAllocator) {
   EXPECT_EQ(totals_.frees_, 0);
 }
 
-TYPED_TEST(SBOVector_, MustConstructSmallNumberOfCopies) {
-  ContainerType container(SMALL_SIZE);
-  EXPECT_EQ(container.size(), SMALL_SIZE);
-  EXPECT_EQ(container.capacity(), SBO_SIZE);
-  EXPECT_FALSE(container.empty());
+TYPED_TEST(SBOVector_, MustConstructCount) {
+  ContainerType small(SMALL_SIZE);
+  EXPECT_EQ(small.size(), SMALL_SIZE);
+  EXPECT_EQ(small.capacity(), SBO_SIZE);
+  EXPECT_FALSE(small.empty());
+  ContainerType large(LARGE_SIZE);
+  EXPECT_EQ(large.size(), LARGE_SIZE);
+  EXPECT_GE(large.capacity(), LARGE_SIZE);
+  EXPECT_FALSE(large.empty());
 }
 
-TEST_F(DataTypeOperationTrackingSBOVector, MustConstructSmallNumberOfCopies) {
+TEST_F(DataTypeOperationTrackingSBOVector, MustConstructCount) {
   {
-    ContainerType container(SMALL_SIZE, create_allocator());
-    EXPECT_EQ(container.size(), SMALL_SIZE);
-    EXPECT_EQ(container.capacity(), SBO_SIZE);
-    EXPECT_FALSE(container.empty());
+    ContainerType small(SMALL_SIZE, create_allocator());
+    EXPECT_EQ(small.size(), SMALL_SIZE);
+    EXPECT_EQ(small.capacity(), SBO_SIZE);
+    EXPECT_FALSE(small.empty());
+    ContainerType large(LARGE_SIZE, create_allocator());
+    EXPECT_EQ(large.size(), LARGE_SIZE);
+    EXPECT_GE(large.capacity(), LARGE_SIZE);
+    EXPECT_FALSE(large.empty());
   }
   EXPECT_EQ(OperationCounter::TOTALS.constructs(),
             OperationCounter::TOTALS.destructs());
   EXPECT_EQ(totals_.allocs_, totals_.frees_);
 }
 
-TEST(ValueVerifiedSBOVector, MustConstructSmallNumberOfCopies) {
-  SBOVector<int, SBO_SIZE> sbo(SMALL_SIZE);
-  std::vector<int> vec(SMALL_SIZE);
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustConstructLargeNumberOfCopies) {
-  ContainerType container(LARGE_SIZE);
-  EXPECT_EQ(container.size(), LARGE_SIZE);
-  EXPECT_GE(container.capacity(), LARGE_SIZE);
-  EXPECT_FALSE(container.empty());
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustConstructLargeNumberOfCopies) {
+TEST(ValueVerifiedSBOVector, MustConstructCount) {
   {
-    ContainerType container(LARGE_SIZE, create_allocator());
-    EXPECT_EQ(container.size(), LARGE_SIZE);
-    EXPECT_GE(container.capacity(), LARGE_SIZE);
-    EXPECT_FALSE(container.empty());
+    SBOVector<int, SBO_SIZE> sbo(SMALL_SIZE);
+    std::vector<int> vec(SMALL_SIZE);
+    EXPECT_RANGE_EQ(sbo, vec);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
+  {
+    SBOVector<int, SBO_SIZE> sbo(LARGE_SIZE);
+    std::vector<int> vec(LARGE_SIZE);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
 }
 
-TEST(ValueVerifiedSBOVector, MustConstructLargeNumberOfCopies) {
-  SBOVector<int, SBO_SIZE> sbo(LARGE_SIZE);
-  std::vector<int> vec(LARGE_SIZE);
-  EXPECT_RANGE_EQ(sbo, vec);
+TEST(ValueVerifiedSBOVector, MustConstructCountValue) {
+  {
+    SBOVector<int, SBO_SIZE> sbo(SMALL_SIZE, 5);
+    std::vector<int> vec(SMALL_SIZE, 5);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    SBOVector<int, SBO_SIZE> sbo(LARGE_SIZE, -13);
+    std::vector<int> vec(LARGE_SIZE, -13);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
 }
 
 TYPED_TEST(SBOVector_, MustConstructFromInitializerList) {
@@ -101,264 +104,179 @@ TEST(ValueVerifiedSBOVector, MustConstructFromInitializerList) {
   EXPECT_RANGE_EQ(sbo, vec);
 }
 
-TYPED_TEST(SBOVector_, MustConstructFromSmallRange) {
-  std::vector<DataType> vec(SMALL_SIZE);
-  ContainerType container(vec.begin(), vec.end());
-  EXPECT_EQ(container.size(), SMALL_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustConstructFromSmallRange) {
+TYPED_TEST(SBOVector_, MustConstructFromRange) {
   {
     std::vector<DataType> vec(SMALL_SIZE);
-    ContainerType container(vec.begin(), vec.end(), create_allocator());
+    ContainerType container(vec.begin(), vec.end());
     EXPECT_EQ(container.size(), SMALL_SIZE);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustConstructFromSmallRange) {
-  std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
-  SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustConstructFromLargeRange) {
-  std::vector<DataType> vec(LARGE_SIZE);
-  ContainerType container(vec.begin(), vec.end());
-  EXPECT_EQ(container.size(), LARGE_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustConstructFromLargeRange) {
   {
     std::vector<DataType> vec(LARGE_SIZE);
-    ContainerType container(vec.begin(), vec.end(), create_allocator());
+    ContainerType container(vec.begin(), vec.end());
     EXPECT_EQ(container.size(), LARGE_SIZE);
   }
+}
+
+TEST_F(DataTypeOperationTrackingSBOVector, MustConstructFromRange) {
+  {
+    {
+      std::vector<DataType> vec(SMALL_SIZE);
+      ContainerType container(vec.begin(), vec.end(), create_allocator());
+      EXPECT_EQ(container.size(), SMALL_SIZE);
+    }
+    {
+      std::vector<DataType> vec(LARGE_SIZE);
+      ContainerType container(vec.begin(), vec.end(), create_allocator());
+      EXPECT_EQ(container.size(), LARGE_SIZE);
+    }
+  }
   EXPECT_EQ(OperationCounter::TOTALS.constructs(),
             OperationCounter::TOTALS.destructs());
   EXPECT_EQ(totals_.allocs_, totals_.frees_);
 }
 
-TEST(ValueVerifiedSBOVector, MustConstructFromLargeRange) {
-  std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
-  SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustCopyConstructSmall) {
-  const ContainerType old(SMALL_SIZE);
-  ContainerType new_(old);
-  EXPECT_EQ(new_.size(), SMALL_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustCopyConstructSmall) {
+TEST(ValueVerifiedSBOVector, MustConstructFromRange) {
   {
-    const ContainerType old(SMALL_SIZE, create_allocator());
+    std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
+    SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
+    SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+}
+
+TYPED_TEST(SBOVector_, MustCopyConstruct) {
+  {
+    const ContainerType old(SMALL_SIZE);
     ContainerType new_(old);
     EXPECT_EQ(new_.size(), SMALL_SIZE);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustCopyConstructSmall) {
-  std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
-  const SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(old);
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustCopyConstructLarge) {
-  const ContainerType old(LARGE_SIZE);
-  ContainerType new_(old);
-  EXPECT_EQ(new_.size(), LARGE_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustCopyConstructLarge) {
   {
-    const ContainerType old(LARGE_SIZE, create_allocator());
+    const ContainerType old(LARGE_SIZE);
     ContainerType new_(old);
     EXPECT_EQ(new_.size(), LARGE_SIZE);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustCopyConstructLarge) {
-  std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
-  const SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(old);
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustCopyConstructAsymetric) {
-  const SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5);
-  ContainerType new_(old);
-  EXPECT_EQ(new_.size(), SBO_SIZE + 5);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustCopyConstructAsymetric) {
   {
-    const SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(
-        SBO_SIZE + 5, create_allocator());
+    const SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5);
     ContainerType new_(old);
     EXPECT_EQ(new_.size(), SBO_SIZE + 5);
+  }
+}
+
+TEST_F(DataTypeOperationTrackingSBOVector, MustCopyConstruct) {
+  {
+    {
+      const ContainerType old(SMALL_SIZE, create_allocator());
+      ContainerType new_(old);
+      EXPECT_EQ(new_.size(), SMALL_SIZE);
+    }
+    {
+      const ContainerType old(LARGE_SIZE, create_allocator());
+      ContainerType new_(old);
+      EXPECT_EQ(new_.size(), LARGE_SIZE);
+    }
+    {
+      const SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(
+          SBO_SIZE + 5, create_allocator());
+      ContainerType new_(old);
+      EXPECT_EQ(new_.size(), SBO_SIZE + 5);
+    }
   }
   EXPECT_EQ(OperationCounter::TOTALS.constructs(),
             OperationCounter::TOTALS.destructs());
   EXPECT_EQ(totals_.allocs_, totals_.frees_);
 }
 
-TEST(ValueVerifiedSBOVector, MustCopyConstructAsymetric) {
-  std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
-  const SBOVector<int, SBO_SIZE + 10> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(old);
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TEST(ValueVerifiedSBOVector, MustCopyConstructAsymetricAllocator) {
-  std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
-  const SBOVector<int, SBO_SIZE + 10, CustomAllocator<int>> old(vec.begin(),
-                                                                vec.end());
-  SBOVector<int, SBO_SIZE> sbo(old);
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustMoveConstructSmall) {
-  ContainerType old(SMALL_SIZE);
-  ContainerType new_(std::move(old));
-  EXPECT_EQ(new_.size(), SMALL_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustMoveConstructSmall) {
+TEST(ValueVerifiedSBOVector, MustCopyConstruct) {
   {
-    ContainerType old(SMALL_SIZE, create_allocator());
+    std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
+    const SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(old);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
+    const SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(old);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
+    const SBOVector<int, SBO_SIZE + 10> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(old);
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+}
+
+TYPED_TEST(SBOVector_, MustMoveConstruct) {
+  {
+    ContainerType old(SMALL_SIZE);
     ContainerType new_(std::move(old));
     EXPECT_EQ(new_.size(), SMALL_SIZE);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustMoveConstructSmall) {
-  std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
-  SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(std::move(old));
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustMoveConstructLarge) {
-  ContainerType old(LARGE_SIZE);
-  ContainerType new_(std::move(old));
-  EXPECT_EQ(new_.size(), LARGE_SIZE);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustMoveConstructLarge) {
   {
-    ContainerType old(LARGE_SIZE, create_allocator());
+    ContainerType old(LARGE_SIZE);
     ContainerType new_(std::move(old));
     EXPECT_EQ(new_.size(), LARGE_SIZE);
   }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustMoveConstructLarge) {
-  std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
-  SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(std::move(old));
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustMoveConstructAsymetric) {
-  SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5);
-  ContainerType new_(std::move(old));
-  EXPECT_EQ(new_.size(), SBO_SIZE + 5);
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustMoveConstructAsymetric) {
   {
-    SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5,
-                                                          create_allocator());
+    SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5);
     ContainerType new_(std::move(old));
     EXPECT_EQ(new_.size(), SBO_SIZE + 5);
+  }
+}
+
+TEST_F(DataTypeOperationTrackingSBOVector, MustMoveConstruct) {
+  {
+    {
+      ContainerType old(SMALL_SIZE, create_allocator());
+      ContainerType new_(std::move(old));
+      EXPECT_EQ(new_.size(), SMALL_SIZE);
+    }
+    {
+      ContainerType old(LARGE_SIZE, create_allocator());
+      ContainerType new_(std::move(old));
+      EXPECT_EQ(new_.size(), LARGE_SIZE);
+    }
+    {
+      SBOVector<DataType, SBO_SIZE + 10, AllocatorType> old(SBO_SIZE + 5,
+                                                            create_allocator());
+      ContainerType new_(std::move(old));
+      EXPECT_EQ(new_.size(), SBO_SIZE + 5);
+    }
   }
   EXPECT_EQ(OperationCounter::TOTALS.constructs(),
             OperationCounter::TOTALS.destructs());
   EXPECT_EQ(totals_.allocs_, totals_.frees_);
 }
 
-TEST(ValueVerifiedSBOVector, MustMoveConstructAsymetric) {
-  std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
-  SBOVector<int, SBO_SIZE + 10> old(vec.begin(), vec.end());
-  SBOVector<int, SBO_SIZE> sbo(std::move(old));
-  EXPECT_RANGE_EQ(sbo, vec);
+TEST(ValueVerifiedSBOVector, MustMoveConstruct) {
+  {
+    std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
+    SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(std::move(old));
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
+    SBOVector<int, SBO_SIZE> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(std::move(old));
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
+  {
+    std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
+    SBOVector<int, SBO_SIZE + 10> old(vec.begin(), vec.end());
+    SBOVector<int, SBO_SIZE> sbo(std::move(old));
+    EXPECT_RANGE_EQ(sbo, vec);
+  }
 }
-
 TEST(ValueVerifiedSBOVector, MustMoveConstructAsymetricAllocator) {
   std::vector<int> vec = make_vector_sequence<SBO_SIZE + 5>();
   SBOVector<int, SBO_SIZE + 10, CustomAllocator<int>> old(vec.begin(),
                                                           vec.end());
   SBOVector<int, SBO_SIZE> sbo(std::move(old));
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustIteratorConstructSmall) {
-  std::vector<DataType> vec(SMALL_SIZE, DataType());
-  ContainerType container(vec.begin(), vec.end());
-  EXPECT_EQ(container.size(), SMALL_SIZE);
-  EXPECT_EQ(container.capacity(), SBO_SIZE);
-  EXPECT_FALSE(container.empty());
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustIteratorConstructSmall) {
-  {
-    std::vector<DataType> vec(SMALL_SIZE, DataType());
-    ContainerType container(vec.begin(), vec.end(), create_allocator());
-    EXPECT_EQ(container.size(), SMALL_SIZE);
-    EXPECT_EQ(container.capacity(), SBO_SIZE);
-    EXPECT_FALSE(container.empty());
-  }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustIteratorConstructSmall) {
-  std::vector<int> vec = make_vector_sequence<SMALL_SIZE>();
-  SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
-  EXPECT_RANGE_EQ(sbo, vec);
-}
-
-TYPED_TEST(SBOVector_, MustIteratorConstructLarge) {
-  std::vector<DataType> vec(LARGE_SIZE, DataType());
-  ContainerType container(vec.begin(), vec.end());
-  EXPECT_EQ(container.size(), LARGE_SIZE);
-  EXPECT_GE(container.capacity(), LARGE_SIZE);
-  EXPECT_FALSE(container.empty());
-}
-
-TEST_F(DataTypeOperationTrackingSBOVector, MustIteratorConstructLarge) {
-  {
-    std::vector<DataType> vec(LARGE_SIZE, DataType());
-    ContainerType container(vec.begin(), vec.end(), create_allocator());
-    EXPECT_EQ(container.size(), LARGE_SIZE);
-    EXPECT_GE(container.capacity(), LARGE_SIZE);
-    EXPECT_FALSE(container.empty());
-  }
-  EXPECT_EQ(OperationCounter::TOTALS.constructs(),
-            OperationCounter::TOTALS.destructs());
-  EXPECT_EQ(totals_.allocs_, totals_.frees_);
-}
-
-TEST(ValueVerifiedSBOVector, MustIteratorConstructLarge) {
-  std::vector<int> vec = make_vector_sequence<LARGE_SIZE>();
-  SBOVector<int, SBO_SIZE> sbo(vec.begin(), vec.end());
   EXPECT_RANGE_EQ(sbo, vec);
 }
